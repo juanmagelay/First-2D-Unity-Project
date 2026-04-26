bff711905decf99b9448a25c9b7777a29cbccb77
Juan Manuel Gelay <juanmanuelgelay@gmail.com>
2026-04-26 20:04:45 -0300

    Changes in CharacterController to shoot with or without skills

--- Assets/_Clase4/Scripts/CharacterController.cs
+++ Assets/_Clase4/Scripts/CharacterController.cs
@@ -5,12 +5,19 @@ public class CharacterControler : MonoBehaviour
     private InputCharacterController input;
     private Movement movement;
     private Shooting shooting;
+    // El "slot" para la habilidad de disparo, puede ser null o apuntar a cualquier método que queramos
+    public System.Action shootAction; // Se necesita public para poder asignarle métodos desde las skills
+
 
     void Awake()
     {
         input = GetComponent<InputCharacterController>();
         movement = GetComponent<Movement>();
         shooting = GetComponent<Shooting>();
+        
+        // Variable para que el character controller no tenga que conocer el shooting si es con o sin habilidad. Le asignamos el método Shot por default, pero podríamos asignar cualquier otro método con la misma firma o dejarlo como null para que no haga nada al disparar
+        shootAction = shooting.Shot; 
+
     }
 
     void Update()
@@ -25,7 +32,9 @@ public class CharacterControler : MonoBehaviour
 
         if (input.ShootPressed)
         {
-            shooting.Shot();
+            // Llama a lo que sea que esté en el slot
+            shootAction?.Invoke();     
         }
+        
     }
 }
